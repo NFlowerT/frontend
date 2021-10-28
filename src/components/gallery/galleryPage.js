c04d03c6558b72eaffbfdc1d5d54397f46dea32d
Maksiu 3 <maks.ga@wp.pl>
2021-10-28 16:17:10 +0200

    ProductPage prototype

--- src/components/gallery/galleryPage.js
+++ src/components/gallery/galleryPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './css/galleryPage.css'
+import { BiSearch } from 'react-icons/bi'
+import { FaBars } from 'react-icons/fa'
 
 const GalleryPage = () => {
     return (
         <main>
-            <div>gallery</div>
+            <section className={'guideSection'}>
+                <div className={'searchbar'}>
+                    <input type={"text"} placeholder={"search"} className={"search"}/>
+                    <BiSearch className={"searchIcon"}></BiSearch>
+                </div>
+                <div className={"filterBar"}>
+                    FILTR <FaBars className={"filterIcon"}></FaBars>
+                </div>
+            </section>
+
         </main>
     )
 }
---
+++ src/components/gallery/productTile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/prodcutTile.css'
+
+
+const ProductTile = () => {
+    return (
+        <div className={"productTile"}>
+
+        </div>
+    )
+}
+
+export default ProductTile
