ec6c3f0bf2ff321346b25808dfa8ba3375a1e828
Maksiu 3 <maks.ga@wp.pl>
2021-12-12 23:10:06 +0100

    CatalogPage

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -24,7 +24,7 @@ const Nav = () => {
                     <Link className={'router ' + (location === '/gallery' ? 'selected' : ' ')} to={'/gallery'}>GALLERY</Link>
                 </li>
                 <li>
-                    <Link className={'router ' + (location === '/catalog' ? 'selected' : ' ')} to={'/catalog'}>CONTACT</Link>
+                    <Link className={'router ' + (location === '/catalog' ? 'selected' : ' ')} to={'/catalog'}>CATALOG</Link>
                 </li>
             </ul>
             <div className={'accountIcon'}>
